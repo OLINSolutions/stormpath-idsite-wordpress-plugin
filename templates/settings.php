592c91077026e567c6c7c5363c45b22404c097fb
Jordan Olin <jordan@olinsolutions.com>
2016-05-03 14:46:18 -0400

    Added width style to input fields on settings.

--- templates/settings.php
+++ templates/settings.php
@@ -23,27 +23,27 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
         <table class="form-table">  
             <tr valign="top">
                 <th scope="row"><label for="sp_apikey_file_location">API Key Properties (only give apache read & execute)</label></th>
-                <td><input type="text" name="sp_apikey_file_location" id="sp_apikey_file_location" value="<?php echo get_option('sp_apikey_file_location'); ?>" /></td>
+                <td><input type="text" style="width: 95%;" name="sp_apikey_file_location" id="sp_apikey_file_location" value="<?php echo get_option('sp_apikey_file_location'); ?>" /></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="sp_directory_href">Directory HREF</label></th>
-                <td><input type="text" name="sp_directory_href" id="sp_directory_href" value="<?php echo get_option('sp_directory_href'); ?>" /></td>
+                <td><input type="text" style="width: 95%;" name="sp_directory_href" id="sp_directory_href" value="<?php echo get_option('sp_directory_href'); ?>" /></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="sp_application_href">Application HREF</label></th>
-                <td><input type="text" name="sp_application_href" id="sp_application_href" value="<?php echo get_option('sp_application_href'); ?>" /></td>
+                <td><input type="text" style="width: 95%;" name="sp_application_href" id="sp_application_href" value="<?php echo get_option('sp_application_href'); ?>" /></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="sp_idsite_login_uri">ID Site Login URI</label></th>
-                <td><input type="text" name="sp_idsite_login_uri" id="sp_idsite_login_uri" value="<?php echo get_option('sp_idsite_login_uri'); ?>" /></td>
+                <td><input type="text" style="width: 95%;" name="sp_idsite_login_uri" id="sp_idsite_login_uri" value="<?php echo get_option('sp_idsite_login_uri'); ?>" /></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="sp_idsite_logout_uri">ID Site Logout URI</label></th>
-                <td><input type="text" name="sp_idsite_logout_uri" id="sp_idsite_logout_uri" value="<?php echo get_option('sp_idsite_logout_uri'); ?>" /></td>
+                <td><input type="text" style="width: 95%;" name="sp_idsite_logout_uri" id="sp_idsite_logout_uri" value="<?php echo get_option('sp_idsite_logout_uri'); ?>" /></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><label for="sp_login_after_logout">Login after Logout?</label></th>
-				<td><input type="radio" name="sp_login_after_logout" id="sp_login_after_logout" <?php if(get_option('sp_login_after_logout') === 'true') echo 'checked'; ?> value="true" />yes 
+				<td><input type="radio" name="sp_login_after_logout" id="sp_login_after_logout" <?php if(get_option('sp_login_after_logout') === 'true') echo 'checked'; ?> value="true" />yes &nbsp;
 				<input type="radio" name="sp_login_after_logout" id="sp_login_after_logout" <?php if(get_option('sp_login_after_logout') === 'false') echo 'checked'; ?> value="false" />no</td>
             </tr>
         </table>
